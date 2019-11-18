9f1036bc7609f5fbbe3ff958e72af0f030db1816
mrb721 <mrb_2059@yahoo.com>
2019-11-18 01:45:02 -0500

    Add files via upload

---
+++ database/inserts.sql
@@ -0,0 +1,11 @@
+use app;
+
+INSERT INTO Person Values('alice123', 'alicekazam', '123', 'alice', 'anderson');
+
+INSERT INTO Person Values('XxXbobXxX', 'bobafett', '456', 'bob', 'bryers');
+
+INSERT INTO Person Values('N0_sC0p3_nancy', 'nonce', '000', 'nancy', 'nolan');
+
+INSERT INTO Content Values (0, 'alice123', '2013-08-05 18:19:03', '/content/alice123/', 'alice is awesome', true);
+
+INSERT INTO Tag Values(1, 'N0_sC0p3_nancy', 'alice123', '2013-08-05 18:19:03', True);
